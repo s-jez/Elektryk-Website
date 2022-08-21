b83911c4ea8f925d99d0632b3e534a96dc015090
s-jez <stanislawjez7@gmail.com>
2022-08-21 19:14:09 +0200

    Code format, remove unsued code, add responsive to video

--- js/main.js
+++ js/main.js
@@ -1,37 +1,36 @@
-
-// Slider 
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const images = document.querySelector('.carousel').children;
+// Slider
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const images = document.querySelector(".carousel").children;
 const totalImages = images.length;
 let index = 0;
 
-prev.addEventListener('click', () => {
-    nextImage('next');
-})
+prev.addEventListener("click", () => {
+  nextImage("next");
+});
 
-next.addEventListener('click', () => {
-    nextImage('prev');
-})
+next.addEventListener("click", () => {
+  nextImage("prev");
+});
 
 function nextImage(direction) {
-    if (direction == 'next') {
-        index++;
-        if (index == totalImages) {
-            index = 0;
-        }
+  if (direction == "next") {
+    index++;
+    if (index == totalImages) {
+      index = 0;
+    }
+  } else {
+    if (index == 0) {
+      index = totalImages - 1;
     } else {
-        if (index == 0) {
-            index = totalImages - 1;
-        } else {
-            index--;
-        }
+      index--;
     }
+  }
 
-    for (let i = 0; i < images.length; i++) {
-        images[i].classList.remove('main');
-    }
-    images[index].classList.add('main');
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove("main");
+  }
+  images[index].classList.add("main");
 }
 // Hamburger menu in progress.. (not working)
 /*let mainNav = document.getElementById("js-menu");
